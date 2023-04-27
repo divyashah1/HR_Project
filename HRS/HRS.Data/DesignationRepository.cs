b43f5b6bf913286906d2ff64c31af56301a544ab
divyashah <divya.shah@prakashinfotech.com>
2023-04-27 16:40:16 +0530

    changes in employee, designation in webapi for login puropse

--- HRS/HRS.Data/DesignationRepository.cs
+++ HRS/HRS.Data/DesignationRepository.cs
@@ -1,12 +1,7 @@
 ﻿using HRS.Busniess.Abstraction;
 using HRS.Busniess.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HRS.Data
 {
